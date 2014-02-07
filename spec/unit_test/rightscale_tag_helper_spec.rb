a885a2f1e0ac92f994aab70cd8d68a8470419009
Kannan Manickam <kannan@rightscale.com>
2014-02-07 12:14:54 -0800

    Finished unit tests for load balancers and application servers.

--- spec/unit_test/rightscale_tag_helper_spec.rb
+++ spec/unit_test/rightscale_tag_helper_spec.rb
@@ -62,14 +62,12 @@ describe Rightscale::RightscaleTag do
         [load_balancer_1, load_balancer_2]
       end
 
-      it 'should return tags from all load balancer servers' do
+      it 'returns tags from all load balancer servers' do
         Chef::MachineTagHelper.should_receive(:tag_search).with(
           node, 'load_balancer:', {:required_tags => Set['server:uuid']}
         ).and_return(tags)
         response = fake.find_load_balancer_servers(node)
 
-        # TODO: Remove me
-        print response.pretty_inspect
         response.should include('01-83PJQDO8911IT')
         response['01-83PJQDO8911IT']['tags'].should eq(load_balancer_1)
         response['01-83PJQDO8911IT']['private_ips'].should eq(['10.0.0.1'])
@@ -81,6 +79,175 @@ describe Rightscale::RightscaleTag do
         response['01-83PJQDO8922IT']['private_ips'].should be_empty
         response['01-83PJQDO8922IT']['public_ips'].should eq(['157.56.166.202', '157.56.166.203'])
         response['01-83PJQDO8922IT']['application_names'].should eq(['api'])
+      end
+    end
+
+    context 'when an application name is specified and the application is available' do
+      let(:tags) do
+        [load_balancer_1, load_balancer_2]
+      end
+
+      it 'returns tags from the matching load balancer server' do
+        Chef::MachineTagHelper.should_receive(:tag_search).with(
+          node, 'load_balancer:active_www=true', {:required_tags => Set['server:uuid']}
+        ).and_return(tags)
+        response = fake.find_load_balancer_servers(node, 'www')
+
+        response.should include('01-83PJQDO8911IT')
+        response['01-83PJQDO8911IT']['tags'].should eq(load_balancer_1)
+        response['01-83PJQDO8911IT']['private_ips'].should eq(['10.0.0.1'])
+        response['01-83PJQDO8911IT']['public_ips'].should eq(['157.56.165.202', '157.56.165.203'])
+        response['01-83PJQDO8911IT']['application_names'].should eq(['www', 'api'])
+      end
+    end
+
+    context 'when an application name is specified and the application is not available' do
+      let(:tags) { [] }
+
+      it 'returns an empty Mash' do
+        Chef::MachineTagHelper.should_receive(:tag_search).with(
+          node, 'load_balancer:active_www=true', {:required_tags => Set['server:uuid']}
+        ).and_return(tags)
+        response = fake.find_load_balancer_servers(node, 'www')
+
+        response.should be_an_instance_of(Mash)
+        response.should be_empty
+      end
+    end
+  end
+
+  describe '.find_application_servers' do
+    let(:application_server_1) do
+      MachineTag::Set[
+        'server:uuid=01-83PJQDO8911IT',
+        'application:active_www=true',
+        'application:active_api=true',
+        'application:bind_ip_address_www=157.56.165.202',
+        'application:bind_ip_address_api=157.56.165.203',
+        'application:bind_port_www=80',
+        'application:bind_port_api=80',
+        'application:vhost_path_www=/',
+        'application:vhost_path_api=api.example.com',
+        'server:public_ip_0=157.56.165.202',
+        'server:public_ip_1=157.56.165.203',
+        'server:private_ip_0=10.0.0.1',
+      ]
+    end
+
+    let(:application_server_2) do
+      MachineTag::Set[
+        'server:uuid=01-83PJQDO8922IT',
+        'application:active_api=true',
+        'application:bind_ip_address_api=157.56.166.202',
+        'application:bind_port_api=443',
+        'application:vhost_path_api=api.example.com',
+        'server:public_ip_0=157.56.166.202',
+        'server:public_ip_1=157.56.166.203',
+      ]
+    end
+
+    let(:www_attributes_1) do
+      Mash.from_hash(
+        'bind_ip_address' => '157.56.165.202',
+        'bind_port' => 80,
+        'vhost_path' => '/'
+      )
+    end
+
+    let(:api_attributes_1) do
+      Mash.from_hash(
+        'bind_ip_address' => '157.56.165.203',
+        'bind_port' => 80,
+        'vhost_path' => 'api.example.com'
+      )
+    end
+
+    let(:api_attributes_2) do
+      Mash.from_hash(
+        'bind_ip_address' => '157.56.166.202',
+        'bind_port' => 443,
+        'vhost_path' => 'api.example.com'
+      )
+    end
+
+    context 'when no application name is specified' do
+      let(:tags) do
+        [application_server_1, application_server_2]
+      end
+
+      it 'returns tags of all application servers' do
+        Chef::MachineTagHelper.should_receive(:tag_search).with(
+          node, 'application:',
+          {:required_tags => Set['server:uuid']}
+        ).and_return(tags)
+        response = fake.find_application_servers(node)
+
+        response.should include('01-83PJQDO8911IT')
+        response['01-83PJQDO8911IT']['tags'].should eq(application_server_1)
+        response['01-83PJQDO8911IT']['private_ips'].should eq(['10.0.0.1'])
+        response['01-83PJQDO8911IT']['public_ips'].should eq(['157.56.165.202', '157.56.165.203'])
+        response['01-83PJQDO8911IT']['applications'].should be_an_instance_of(Mash)
+        response['01-83PJQDO8911IT']['applications']['www'].should eq(www_attributes_1)
+
+        response.should include('01-83PJQDO8922IT')
+        response['01-83PJQDO8922IT']['tags'].should eq(application_server_2)
+        response['01-83PJQDO8922IT']['private_ips'].should eq([])
+        response['01-83PJQDO8922IT']['public_ips'].should eq(['157.56.166.202', '157.56.166.203'])
+        response['01-83PJQDO8922IT']['applications'].should be_an_instance_of(Mash)
+        response['01-83PJQDO8922IT']['applications']['api'].should eq(api_attributes_2)
+      end
+    end
+
+    context 'when an application name is specified and the application is available' do
+      let(:tags) do
+        [application_server_1, application_server_2]
+      end
+
+      it 'returns tags of matching application servers' do
+        Chef::MachineTagHelper.should_receive(:tag_search).with(
+          node, 'application:active_www=true',
+          {:required_tags => Set[
+            'server:uuid',
+            'application:bind_ip_address_www=*',
+            'application:bind_port_www=*',
+            'application:vhost_path_www=*'
+          ]}
+        ).and_return(tags)
+        response = fake.find_application_servers(node, 'www')
+
+        response.should include('01-83PJQDO8911IT')
+        response['01-83PJQDO8911IT']['tags'].should eq(application_server_1)
+        response['01-83PJQDO8911IT']['private_ips'].should eq(['10.0.0.1'])
+        response['01-83PJQDO8911IT']['public_ips'].should eq(['157.56.165.202', '157.56.165.203'])
+        response['01-83PJQDO8911IT']['applications'].should be_an_instance_of(Mash)
+        response['01-83PJQDO8911IT']['applications']['www'].should eq(www_attributes_1)
+
+        response.should include('01-83PJQDO8922IT')
+        response['01-83PJQDO8922IT']['tags'].should eq(application_server_2)
+        response['01-83PJQDO8922IT']['private_ips'].should eq([])
+        response['01-83PJQDO8922IT']['public_ips'].should eq(['157.56.166.202', '157.56.166.203'])
+        response['01-83PJQDO8922IT']['applications'].should be_an_instance_of(Mash)
+        response['01-83PJQDO8922IT']['applications']['api'].should eq(api_attributes_2)
+      end
+    end
+
+    context 'when an application name is specified and the application is not available' do
+      let(:tags) { [] }
+
+      it 'returns an empty Mash' do
+        Chef::MachineTagHelper.should_receive(:tag_search).with(
+          node, 'application:active_www=true',
+          {:required_tags => Set[
+            'server:uuid',
+            'application:bind_ip_address_www=*',
+            'application:bind_port_www=*',
+            'application:vhost_path_www=*'
+          ]}
+        ).and_return(tags)
+        response = fake.find_application_servers(node, 'www')
+
+        response.should be_an_instance_of(Mash)
+        response.should be_empty
       end
     end
   end
