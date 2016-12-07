865547dbd19a86e8a098bae5106163de00661dc4
Richard Shade <rshade@rightscale.com>
2016-12-07 09:39:43 -0600

    adding source_url, and issues_url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Provides LWRPs and helper methods for building 3-tier applications using machine tags in RightScale'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
+issues_url       'https://github.com/rightscale-cookbooks/rightscale_tag/issues'
+source_url       'https://github.com/rightscale-cookbooks/rightscale_tag'
 
 depends 'machine_tag', '~> 2.0'
 depends 'marker', '~> 2.0.0'
