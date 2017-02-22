7fd2d0f316c7d3f452b52e37a7ca99f4e8e4ea83
gonzalez <edds718@gmail.com>
2017-02-22 09:49:20 -0500

    changelog and version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ rightscale_tag Cookbook CHANGELOG
 
 This file is used to list changes made in each version of the rightscale_tag cookbook.
 
+v2.0.2
+------
+
+- Adding a generic application server tagging recipe
+
 v2.0.1
 ------
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Provides LWRPs and helper methods for building 3-tier applications using machine tags in RightScale'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 issues_url       'https://github.com/rightscale-cookbooks/rightscale_tag/issues'
 source_url       'https://github.com/rightscale-cookbooks/rightscale_tag'
 
